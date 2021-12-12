b63fae2c1911bfc8a96f190f565c0b5757998674
Wooseok Han <wswj9608@naver.com>
2021-12-12 16:55:48 +0900

    Add : 베르트랑 공준

--- Lecture/wooseokgimozzi/week_3/01_selection_sort.py
+++ Lecture/wooseokgimozzi/week_3/01_selection_sort.py
@@ -2,15 +2,14 @@ input = [4, 6, 2, 9, 1]
 
 
 def selection_sort(array):
-  n = len(array)
+  n = len(array) # 
   for i in range(n - 1) :
     min_index = i
     for j in range(n - i):
       if array[i + j] < array[min_index] :
         min_index = i + j
-      print(i+j)
-    array[i + j], array[min_index] = array[min_index], array[i + j]
-  return
+    array[i], array[min_index] = array[min_index], array[i]
+  return 
 
 
 selection_sort(input)
---
+++ Lecture/wooseokgimozzi/week_3/03_merge_sort.py
@@ -0,0 +1,14 @@
+array_a = [1, 2, 3, 5]
+array_b = [4, 6, 7, 8]
+
+
+def merge(array1, array2):
+  array_c = []
+  array1_idx = 0
+  array2_idx = 0
+  while array1_idx < len(array1) :
+    
+    return
+
+
+print(merge(array_a, array_b))  # [1, 2, 3, 4, 5, 6, 7, 8] 가 되어야 합니다!
